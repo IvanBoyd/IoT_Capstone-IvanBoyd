2c9b2f6ac7eadb1c516cdd044eec406456929cb5
Ivan Boyd <iboyd3@cnm.edu>
2021-12-02 15:23:45 -0700

    still cleaning up U2_LIDAR

--- U2_Lidar2NeoPixel/src/U2_Lidar2NeoPixel.ino
+++ U2_Lidar2NeoPixel/src/U2_Lidar2NeoPixel.ino
@@ -14,20 +14,22 @@
 #define PIXEL_TYPE WS2812B
 bool  dBug    = true;
 int   dBugDel = 300;
-//                              ***  NEOPIXEL SETUP  ***
+//                              ***  NEOPIXEL HEADER  ***
 const int NEOPIXPIN     = A3;
 const int NEOPIXEL_NUM  = 12;
 int   i, j;         // NeoPix brightness 0-155, 145 bout high enuf
 int   minNP = 0,          maxNP = 150,                         // lower/upper NP intensity
       low = 3,            med = 50,         high = 127;        // for NP light intensity - var to hold mapped value from dist
-int   maxDist = 600,      minDist = 1;                         // max must be less than any object that will constanly trigger
-bool  inRange;
+int someColors[] = {0xFF0000, 0x0000FF, 0x8000080, 0xFFFF00, 0xFF0FF, 0x808080, 0xFFA500, 0xA52A2A, 0x008000, 0x808000};
+                  //  RED,    BLUE,     PURPLE,     YELLOW,   MAGENTA,   GRAY,    ORANGE,  BROWN,    GREEN,   OLIVE
 Adafruit_NeoPixel NEO_Pix(NEOPIXEL_NUM, NEOPIXPIN, PIXEL_TYPE);
 
-//                              ***  LIDAR SETUP  ***
+//                              ***  LIDAR HEADER  ***
 LIDARLite_v4LED myLidarLite;
-uint8_t  newDistance,      NPintensity   = 0,  newNPintensity;
-uint16_t distance,         NPdistance;
+uint8_t   newDistance,      NPintensity   = 0,  newNPintensity;
+uint16_t  distance,         NPdistance;
+int       maxDist = 600,    minDist = 1;                         // max must be less than any object that will constanly trigger
+bool      inRange;
 #define FAST_I2C
 #define MonitorPin    SDA        //   SDA, pin 0  Yellow wire   LIDAR-Lite I2C SDA 
 #define TriggerPin    SCL        //   SCL, pin 1  Green wire    LIDAR-Lite I2C SCL 
@@ -41,32 +43,21 @@ void setup()
 {
   Serial.begin(9600);
   waitFor(Serial.isConnected, 15000); 
-  Wire.begin();            // Initialize  I2C (for communication to LidarLite)
-  NEO_Pix.begin();
-  NEO_Pix.setBrightness(med); 
-  for (i = 0; i < NEOPIXEL_NUM; i++)  {
-    NEO_Pix.setPixelColor(i, 0xFF0000);   // turn off NeoPixel
-    NEO_Pix.show(); 
-  }
-  delay(1000); 
-  NEO_Pix.clear();   // turn off NeoPixel
-  NEO_Pix.show(); 
-  minDist = 2; maxDist = 3; minNP = 2; maxNP = 155;
+  Wire.begin();                   // Initialize  I2C (for communication to LidarLite)
+  npSetUp();//                              ***  runs NEOPIXEL SETUP function ***
+  minDist = 2; maxDist = 650; minNP = 2; maxNP = 155;     // maxDist in cm, 1 meter = 100 cm, 39'ish inches & 1.1 yards 
+
 }
 //  *****************   E N D    S E T U P    **********
 
 
 //  *****************  B E G I N     M A I N    V O I D    L O O P    **********
 void loop()   {
-  // NEO_Pix.clear();
-  // NEO_Pix.show();     // do Show to affect pixel color change (to off)
   newDistance = distanceSingle(&distance);
-  NPdistance = distance;
-  if ((NPdistance > 0) && (NPdistance < maxDist))   {
-    inRange = true;
-  }
-  else if (!((NPdistance > 0) && (NPdistance < maxDist)))   {
-    inRange = false;
+  NPdistance  = distance;
+  inRange = ((NPdistance > 0) && (NPdistance < maxDist));
+  if (!((NPdistance > 0) && (NPdistance < maxDist)))   {                  // object is not in range
+      inRange = false;
   }
   if (!inRange)  {
     if(dBug) {Serial.printf("*NOT* inRange: %i Distance = %icm    NPDistance = %icm\n", inRange, distance, NPdistance); delay(dBugDel);} 
@@ -77,33 +68,47 @@ void loop()   {
     if(dBug) {Serial.printf("** inRange **: %i, Distance = %icm    NPDistance = %icm\n", inRange, distance, NPdistance); delay(dBugDel);} 
     if (NPdistance > maxDist) {
         NPdistance = maxDist;
-      }
-      // NPintensity = map(distance, minDist, maxDist, minNP, maxNP);     // map(value, fromLow, fromHigh, toLow, toHigh)
-      NPintensity = map(NPdistance, maxDist, minDist, minNP, maxNP);     // map(value, fromLow, fromHigh, toLow, toHigh)
-      // NPintensity = map(newDistance, 2, 1000, 1, 150);
-      // NPintensity = map(distance, maxDist, 0, 0, 127);              // values used in sensor2Light program (maxDist=98) for reversing the LED when getting closer
-      Serial.printf("Mapping Dist to NPs, Dist: %i NPdist: %i NPintensity: %i minDist: %i maxDist %i minNP %i maxNP %i  \n", distance, NPdistance, NPintensity, minDist, maxDist, minNP, maxNP);
-      // delay(1000);
-      NEO_Pix.setBrightness(NPintensity); 
-      for (i = 0; i < NEOPIXEL_NUM; i++)  {
-        NEO_Pix.setPixelColor(i, 0xFF0000);   // set to red
-      }
+    }
+    // NPintensity = map(distance, minDist, maxDist, minNP, maxNP);      // map(value, fromLow, fromHigh, toLow, toHigh)
+    NPintensity = map(NPdistance, maxDist, minDist, minNP, maxNP/2);     // map(value, fromLow, fromHigh, toLow, toHigh)
+    // NPintensity = map(newDistance, 2, 1000, 1, 150);
+    // NPintensity = map(distance, maxDist, 0, 0, 127);                  // values used in sensor2Light program (maxDist=98) for reversing the LED when getting closer
+    Serial.printf("Mapping Dist to NPs, Dist: %i NPdist: %i NPintensity: %i minDist: %i maxDist %i minNP %i maxNP %i  \n", distance, NPdistance, NPintensity, minDist, maxDist, minNP, maxNP);
+    NEO_Pix.setBrightness(NPintensity); 
+    for (i = 0; i < NEOPIXEL_NUM; i++)  {
+      NEO_Pix.setPixelColor(i, 0xFF0000);                                // set to red
+    }
     NEO_Pix.show(); 
     if (newDistance) {
       Serial.println(distance);
     }
-  delay(500);
+  if(dBug) {delay(500);}
   }
 }
+//  *****************  E N D     M A I N    V O I D    L O O P    **********
 
 
-//---------------------------------------------------------------------
-// Read Single Distance Measurement
-//
-// This is the simplest form of taking a measurement. This is a
-// blocking function as it will not return until a range has been
-// taken and a new distance measurement can be read.
-//---------------------------------------------------------------------
+//  *****************    B E G I N     F U N C T I O N S    **********
+
+void npSetUp()  {
+  NEO_Pix.begin();
+  NEO_Pix.setBrightness(5); 
+  for (i = 0; i < NEOPIXEL_NUM; i++)  {
+    NEO_Pix.setPixelColor(i, someColors[random(0,9)]);
+    NEO_Pix.show(); 
+    delay(75);
+  }
+  delay(2000); 
+  NEO_Pix.clear();                // turn off NeoPixel
+  NEO_Pix.show(); 
+}
+
+// bool notInRange(bool _inRange)  {
+//   return(_inRange = false);
+// }
+
+
+// function to read distance
 uint8_t distanceSingle(uint16_t *distance)  {
     myLidarLite.takeRange();                      // 1. Trigger range measurement.
     myLidarLite.waitForBusy();                    // 2. Wait for busyFlag to indicate device is idle.
